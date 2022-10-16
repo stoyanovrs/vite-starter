2dae72faa1502400426cd67b0b7a5ca3b4d7456c
stoyanovrs <stoyanovrs@gmail.com>
2022-10-16 12:25:53 +0300

    Start Point

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log('Hello Vite');
+
+let checkbox = document.querySelector('input[name=theme_switch]');
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.setAttribute('data-theme', 'dark');
+  checkbox.checked = true;
+} else {
+  document.documentElement.setAttribute('data-theme', 'light');
+  checkbox.checked = false;
+}
+
+// switch theme if checkbox is engaged
+checkbox.addEventListener('change', (cb) => {
+  document.documentElement.setAttribute(
+    'data-theme',
+    cb.target.checked ? 'dark' : 'light'
+  );
+});
+
+// Toggle Button
+let toggle = document.querySelector('.toggle');
+let mainNav = document.querySelector('.main-nav');
+
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('open');
+  mainNav.classList.toggle('show');
+})
